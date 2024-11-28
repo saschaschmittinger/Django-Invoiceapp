d55f3331da4206b5ecae3060c2b8c7424c24bf21
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-28 10:02:27 +0100

    28_11_24

--- receivers/admin.py
+++ receivers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Receiver
 
-# Register your models here.
+
+@admin.register(Receiver)
+class ReceiverAdmin(admin.ModelAdmin):
+    fields: list = ["name", "adresse", "webseite", "erstellt"]
+    list_display: list = ["name", "adresse", "erstellt"]
--- receivers/models.py
+++ receivers/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 
 class Receiver(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    adresse = models.TextField()
+    webseite = models.URLField(blank=True)
+    erstellt = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return str(self.name)
