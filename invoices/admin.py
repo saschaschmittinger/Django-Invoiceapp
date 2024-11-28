b0563779d506b1c2576516e54439dff0191dad4a
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-28 10:53:49 +0100

    28_11_24

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Invoice
 
-# Register your models here.
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display: list = [
+        "empfänger",
+        "rechnungsnummer",
+        "zahlungsziel",
+        "abgeschlossen",
+    ]
+    fields: list = [
+        "profil",
+        "empfänger",
+        "rechnungsnummer",
+        "erfüllungsdatum",
+        "rechnungsdatum",
+        "zahlungsziel",
+        "erstellt",
+        "abgeschlossen",
+    ]
+    readonly_fields: list = ["erstellt"]
--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from profiles.models import Profile
+from receivers.models import Receiver
 
-# Create your models here.
+
+class Invoice(models.Model):
+    profil = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    empfänger = models.ForeignKey(Receiver, on_delete=models.CASCADE)
+    rechnungsnummer = models.CharField(max_length=150)
+    erfüllungsdatum = models.DateField()
+    rechnungsdatum = models.DateField()
+    zahlungsziel = models.DateField()
+    erstellt = models.DateTimeField(auto_now_add=True)
+    abgeschlossen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Rechnungsnummer: {self.rechnungsnummer}, pk: {self.pk}"
+
+    def get_positions(self):
+        pass
+
+    def get_total_amount(self):
+        pass
