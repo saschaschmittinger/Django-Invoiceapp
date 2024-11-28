b758cd5ea113b08ead1c5aa0461b1c1e6f06b2e3
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-28 09:44:26 +0100

    28_11_24

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display: list = ["firmen_name", "user", "erstellt", "update"]
+    list_filter: list = ["firmen_name", "erstellt", "user"]
+    fields: list = [
+        "user",
+        "account_nummer",
+        "firmen_name",
+        "firmen_info",
+        "erstellt",
+        "update",
+    ]
+    readonly_fields: list = ["erstellt", "update"]
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_nummer = models.CharField(max_length=26, blank=True)
+    firmen_name = models.CharField(max_length=250)
+    firmen_info = models.TextField()
+    erstellt = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
