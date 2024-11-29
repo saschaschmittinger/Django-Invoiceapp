6d3cde503cb183c3c31914f6f1517c0550339c26
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-29 12:24:09 +0100

    29_11_24

--- receivers/admin.py
+++ receivers/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportActionMixin
 from .models import Receiver
 
 
+class ReceiverResources(resources.ModelResource):
+    class Meta:
+        model = Receiver
+        fields = {"id", "name", "adresse", "webseite", "erstellt"}
+        export_order = ("id", "name", "adresse", "webseite", "erstellt")
+
+
 @admin.register(Receiver)
-class ReceiverAdmin(admin.ModelAdmin):
-    fields: list = ["name", "adresse", "webseite", "erstellt"]
+class ReceiverAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = ReceiverResources
     list_display: list = ["name", "adresse", "erstellt"]
