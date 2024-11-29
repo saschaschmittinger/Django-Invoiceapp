4b5399bc2ff4fe412b53fce6c6cc57146e7414fc
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-29 11:59:53 +0100

    29_11_24

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from import_export import resources
+from import_export.fields import Field
 from import_export.admin import ExportActionMixin
 from .models import Profile
 
 
 class ProfileRecources(resources.ModelResource):
+    user = Field()
+
     class Meta:
         model = Profile
-        fields = {"id", "user"}
+        fields = {
+            "id",
+            "user",
+            "kontonummer",
+            "firmen_name",
+            "firmen_info",
+            "erstellt",
+            "update",
+        }
+
+    def dehydrate_user(self, obj):
+        return obj.user.username
 
 
 @admin.register(Profile)
