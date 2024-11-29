a2003fb08cfcd0dd898b0b24faeafeb4acc186a3
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-29 22:43:45 +0100

    29_11_24

--- positions/admin.py
+++ positions/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.fields import Field
+from import_export.admin import ExportActionMixin
 from .models import Position
 
 
+class PositionResource(resources.ModelResource):
+    erstellt = Field()
+    rechnung = Field()
+    beschreibung = Field()
+
+    class Meta:
+        model = Position
+        fields = {"id", "leistung", "rechnung", "beschreibung", "betrag", "erstellt"}
+
+    def dehydrate_rechnung(self, obj):
+        return obj.rechnung.rechnungsnummer
+
+    def dehydrate_erstellt(self, obj):
+        return obj.erstellt.strftime("%d-%m-%y")
+
+    def dehydrate_beschreibung(self, obj):
+        if obj.beschreibung == "":
+            return "-"
+        return obj.beschreibung
+
+
 @admin.register(Position)
-class PositionAdmin(admin.ModelAdmin):
+class PositionAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PositionResource
     list_display: list = ["rechnung", "leistung", "betrag", "erstellt"]
     list_filter: list = ["rechnung", "erstellt"]
-    fields: list = ["rechnung", "leistung", "beschreibung", "betrag", "erstellt"]
-    readonly_fields: list = ["erstellt"]
