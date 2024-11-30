fb1a689a7470f021f5fe5dc7b508a44f47626c77
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-30 11:56:42 +0100

    30_11_24

--- invoices/views.py
+++ invoices/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from invoices.models import Invoice
+from profiles.models import Profile
 from django.views import generic
 
 
 class Home_base_view(generic.ListView):
     title: str = "SSC Consult"
-    template_name = "base.html"
-    queryset = Invoice.objects.all()
-    context_object_name = "invoices"
+    template_name = "invoices/index.html"
+    model = Invoice
+    context_object_name = "qs"
+
+    def get_queryset(self):
+        profil = get_object_or_404(Profile, user=self.request.user)
+        return super().get_queryset().filter(profil=profil).order_by("-erstellt")
 
     def get_context_data(self, **kwargs):
         context = super(Home_base_view, self).get_context_data(**kwargs)
