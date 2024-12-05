13f8a04385b32249e96bb571f3d19f829dd2cd6e
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-12-05 11:01:47 +0100

    05_12_24

--- positions/forms.py
+++ positions/forms.py
@@ -6,3 +6,15 @@ class PositionForm(forms.ModelForm):
     class Meta:
         model = Position
         fields = ("leistung", "beschreibung", "betrag")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["leistung"].label = (
+            "<span class='label-erfüllungsdatum'>Leistung</span>"
+        )
+        self.fields["beschreibung"].label = (
+            "<span class='label-erfüllungsdatum'>Beschreibung</span>"
+        )
+        self.fields["betrag"].label = (
+            "<span class='label-erfüllungsdatum'>Betrag</span>"
+        )
